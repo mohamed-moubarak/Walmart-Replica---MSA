da48299512faa72f8ec0ae0672d5f73ec58ea824
HeshamW8 <heshamwardany@gmail.com>
2017-04-19 23:14:36 +0200

    deleted unecessary bat files

--- src/rabbitclient/sender.java
+++ src/rabbitclient/sender.java
@@ -5,17 +5,17 @@ import com.rabbitmq.client.ConnectionFactory;
 
 public class sender {
 
-  private final static String QUEUE_NAME = "hello";
+  private final static String QUEUE_NAME = "userApp";
 
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
     factory.setHost("localhost");
-    factory.setPort(5000);
+		// factory.setPort(5672);
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
 
     channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-    String message = "Hello World!";
+    String message = "Second Message";
     channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
     System.out.println(" [x] Sent '" + message + "'");
 
