780fe530bbbdd990931e069816af962cdb7163bd
HeshamW8 <heshamwardany@gmail.com>
2017-05-02 18:09:15 +0200

    rabbitClient added

--- src/rabbitclient/RabbitClient.java
+++ src/rabbitclient/RabbitClient.java
@@ -9,10 +9,12 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
 
-public class receiver {
+public class RabbitClient {
 
-	private final static String QUEUE_NAME = "hello";
+	private final static String QUEUE_NAME = "userApp";
 
 	public static String executePost(String targetURL, String urlParameters) {
 		HttpURLConnection connection = null;
@@ -21,26 +23,27 @@ public class receiver {
 			// Create connection
 			URL url = new URL(targetURL);
 			connection = (HttpURLConnection) url.openConnection();
+			connection.setDoOutput(true);
+			connection.setInstanceFollowRedirects(false);
 			connection.setRequestMethod("POST");
 			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 			connection.setRequestProperty("Accept", "application/json");
-
 			connection.setRequestProperty("Content-Length", Integer.toString(urlParameters.getBytes().length));
-			connection.setRequestProperty("Content-Language", "en-US");
-
+			// connection.setRequestProperty("Content-Language", "en-US");
+			connection.setRequestProperty("charset", "utf-8");
 			connection.setUseCaches(false);
-			connection.setDoOutput(true);
+
+			byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
 
 			// Send request
 			DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-			wr.writeBytes("SrvReq:" + urlParameters);
+			wr.write(postData);
 			wr.close();
 
 			// Get Response
 			InputStream is = connection.getInputStream();
 			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-			StringBuilder response = new StringBuilder(); // or StringBuffer if
-															// Java version 5+
+			StringBuilder response = new StringBuilder();
 			String line;
 			while ((line = rd.readLine()) != null) {
 				response.append(line);
@@ -48,6 +51,7 @@ public class receiver {
 			}
 			rd.close();
 			return response.toString();
+			// return null;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
@@ -58,7 +62,24 @@ public class receiver {
 		}
 	}
 
+	public static void publisher() throws IOException, TimeoutException {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		factory.setPort(5673);
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+		String message = "dummy message";
+
+		channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+	}
+
 	public static void main(String[] argv) throws Exception {
+
+		// publisher();
+
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost("localhost");
 		Connection connection = factory.newConnection();
@@ -73,10 +94,13 @@ public class receiver {
 					byte[] body) throws IOException {
 				String message = new String(body, "UTF-8");
 				System.out.println(" [x] Received '" + message + "'");
-				executePost("http://localhost:8080", message);
+
+				executePost("http://localhost:8080", "SrvReq=" + message);
+
 			}
 		};
 
 		channel.basicConsume(QUEUE_NAME, true, consumer);
 	}
+
 }
